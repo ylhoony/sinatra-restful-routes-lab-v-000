09b401d9e0501f368063941b97309c9a01a9a644
Hoon Lee <yl.hoony@gmail.com>
2018-01-09 20:01:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,8 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/recipes/:id/delete' do
-    @recipe = Recipe.find_by(id: params[:id])
-    @recipe.delete
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.destroy
     redirect '/recipes'
   end
 
