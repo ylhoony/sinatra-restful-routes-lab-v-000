f0b6fcb45127afbdcd617a66f1f241795b8c6556
Hoon Lee <yl.hoony@gmail.com>
2018-01-09 18:57:13 +0000

    automatically backed up by learn

--- db/migrate/20180109185309_create_recipes.rb
+++ db/migrate/20180109185309_create_recipes.rb
@@ -1,7 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def up
     create_tables :recipes do |t|
-      
+      t.string :name
+      t.text :ingredients
+      t.cook_time 
     end
   end
 end
