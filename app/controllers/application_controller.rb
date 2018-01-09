370544b032aad2368ba632b02b2b25892aa6617c
Hoon Lee <yl.hoony@gmail.com>
2018-01-09 19:16:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,14 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
 
   get '/recipes/:id' do
-
+    @recipe = Recipe.find_by(id: params[:id])
   end
 
   delete '/recipes/:id/delete' do
